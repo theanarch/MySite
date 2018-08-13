8a65681ea8ec9f03d2c4d7953392104e2a11b4fe
theanarch <boris.torbica@gmail.com>
2018-08-14 01:27:25 +0200

    Navbar functionality. Fixed overall layout.

--- MyCV/Controllers/OtherController.cs
+++ MyCV/Controllers/OtherController.cs
@@ -3,16 +3,10 @@
 
 namespace MyCV.Controllers
 {
-    
-    
+
+
     public class OtherController : Controller
     {
-        
-        public ActionResult Faq()
-        {
-            return View();
-        }
-        
         public ActionResult FourOhFour()
         {
             return View();
--- MyCV/Controllers/PortfolioController.cs
+++ MyCV/Controllers/PortfolioController.cs
@@ -8,10 +8,33 @@ namespace MyCV.Controllers
     public class PortfolioController : Controller
     {
         [HttpGet]
-        public ActionResult OneColumn()
+        public ActionResult RaspberryPiGeneral()
         {
             return View();
         }
-        
+
+        [HttpGet]
+        public ActionResult IOSGeneral()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TodoListiOS()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ArduinoGeneral()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult WebDevGeneral()
+        {
+            return View();
+        }
     }
 }
